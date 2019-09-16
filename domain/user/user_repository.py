ae231d351e08422a2718b86d624fbf919eb57b74
Zaki Indra Sukma <indra.zaki@gmail.com>
2019-09-16 09:52:13 +0530

    Fix user attribute order

--- domain/user/user_repository.py
+++ domain/user/user_repository.py
@@ -57,5 +57,4 @@ class UserRepository:
 
     def _row_to_user(self, row):
         print(row)
-        return User(row[0], row[2], row[1], row[3], row[4], row[5], row[6])
-        # return User(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+        return User(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
